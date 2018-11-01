45566eb10c867004a41539ccd347839d9f057ed7
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2018-11-01 21:53:39 +0100

    Add /api/users/me endpoint and jwt endpoint.

--- backend/pah_fm/urls.py
+++ backend/pah_fm/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 
 from rest_framework_jwt.views import obtain_jwt_token
 
+from fleet_management.api import CurrentUserRetrieveView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_jwt_token),
+    path('api/api-token-auth/', obtain_jwt_token),
+    path('api/users/me', CurrentUserRetrieveView.as_view(), name='me'),
 ]
