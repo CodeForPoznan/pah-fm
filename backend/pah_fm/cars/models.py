854fcfc065a9d2d4c7d3952051a3a148c96feabd
ksawery <ksawery.fryczynski.pp>
2018-11-07 21:21:15 +0100

    Merge branch 'master' into work_ksawery
    # Conflicts:
    #	Makefile
    #	backend/pah_fm/urls.py
    #	backend/requirements/base.txt
    #	frontend/README.md
    #	frontend/package-lock.json
    #	frontend/src/App.vue
    #	frontend/src/components/Header.vue
    #	frontend/src/views/LoginView.vue
    #	frontend/vue.config.js

--- backend/pah_fm/cars/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class CarModel(models.Model):
-    plates = models.CharField(max_length=10, unique=True, blank=False)
-
-    def __str__(self):
-        return self.plates
--- backend/pah_fm/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class UserProfile(AbstractUser):
-
-    def __str__(self):
-        return self.username
