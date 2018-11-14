534c03c49f7d8c9b64476458029c125b435ae192
Adam Gajzlerowicz <nelf86@gmail.com>
2018-11-14 18:01:15 +0100

    #1 added refresh button to service worker (#46)
    * added refresh button to service woker
    
    * updated after qc
    
    * added eslint rule
    
    * styling
    
    * added deps
    
    * coma

--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -1,6 +1,7 @@
 export const SET_USER = 'SET_USER';
 export const SET_LOGIN_PROGRESS = 'SET_LOGIN_PROGRESS';
 export const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
+export const SET_UPDATE_READY = 'SET_UPDATE_READY';
 
 export const mutations = {
   [SET_USER](state, user) {
@@ -11,5 +12,8 @@ export const mutations = {
   },
   [SET_LOGIN_ERROR](state, loginError) {
     Object.assign(state, { loginError });
+  },
+  [SET_UPDATE_READY](state, isReady) {
+    Object.assign(state, { updateReady: isReady });
   },
 };
