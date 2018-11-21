4cfd5d02c9f850968d16b4beb8ca6f05061c0781
Adam Gajzlerowicz <nelf86@gmail.com>
2018-11-21 06:23:49 +0100

    heading

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer p-3">
-    <h3 class="h3"><a href="http://codeforpoznan.pl">Code for Poznan</a></h3>
+    <h3 class="heading"><a href="http://codeforpoznan.pl">Code for Poznan</a></h3>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
 <style scoped lang="scss">
 @import "../scss/base";
 
-.h3 {
+.heading {
   margin: 0;
 }
 
