7c28fb455e8622abff3e5c2754bef3bd4f5cdceb
Adam Gajzlerowicz <nelf86@gmail.com>
2018-11-21 18:16:25 +0100

    better fix

--- backend/fleet_management/models.py
+++ backend/fleet_management/models.py
@@ -32,7 +32,7 @@ class Passenger(models.Model):
     last_name = models.CharField(max_length=60, blank=False)
 
     def __str__(self):
-        return '{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
 
 
 class Route(models.Model):
