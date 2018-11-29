3a3b6194fc5a81179ce78fc53ab6ee702791120e
Łukasz Materla <lukasz.materla91@gmail.com>
2018-11-29 16:05:08 +0100

    Language switcher added with correct style. (#80)
    * Language switcher added with correct style.
    Switcher working with EN, PL, UKR
    
    * Added cursor pointer css style to svg element
    
    * Correct errors
    
    * Added icon-flags library to project
    
    * Changed font size of icons flag
    
    * Changed EN to GB for english languages

---
+++ frontend/src/components/Language.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul
+      class="lang">
+      <li
+        v-for="language in languagesOrder"
+        :key="language"
+        @click="changeLang(language)">
+        <flag
+          :iso="language" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import { languagesOrder } from '../main';
+
+/* eslint-disable no-underscore-dangle */
+export default {
+  name: 'Language',
+  methods: {
+    changeLang(languageChecked) {
+      this._i18n.locale = languageChecked;
+    },
+  },
+  data() {
+    return {
+      languagesOrder,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+ul.lang {
+  list-style-type: none;
+  float: right;
+  padding-left: 10px;
+  cursor: pointer;
+}
+
+.lang li {
+  display: inline;
+  font-size: 30px;
+  margin-left: 20px;
+}
+</style>
