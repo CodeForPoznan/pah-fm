0d744ec92fa73029fe88a4518ee3f52f88972cbc
Jacek Bronski-Jankowski <jacek.j.jankowski@gmail.com>
2018-12-28 12:14:35 +0100

    #66 Add uuid field to Django admin for VerificationToken model.

--- backend/fleet_management/admin.py
+++ backend/fleet_management/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Car, Passenger, Drive, User, Project
+
+from .models import Car, Passenger, Drive, User, Project, VerificationToken
 
 
 admin.site.register(Car)
@@ -7,3 +8,8 @@ admin.site.register(Passenger)
 admin.site.register(Drive)
 admin.site.register(User)
 admin.site.register(Project)
+
+
+@admin.register(VerificationToken)
+class VerificationTokenAdmin(admin.ModelAdmin):
+    readonly_fields = ('token', )
