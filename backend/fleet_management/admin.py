42aedaab1248f1316c4127effed13157c3281d29
Jacek Bronski-Jankowski <jacek.j.jankowski@gmail.com>
2018-12-29 12:47:57 +0100

    #66 Add fields do Django admin VerificationToken list view.

--- backend/fleet_management/admin.py
+++ backend/fleet_management/admin.py
@@ -12,4 +12,15 @@ admin.site.register(Project)
 
 @admin.register(VerificationToken)
 class VerificationTokenAdmin(admin.ModelAdmin):
+
+    def is_active(self, instance):
+        return instance.is_active
+
+    def driver(self, instance):
+        return instance.drive.driver.get_full_name()
+
+    is_active.boolean = True
+
     readonly_fields = ('token', )
+    list_display = ('passenger', 'driver', 'is_confirmed', 'is_ok', 'is_active', 'created_at')
+    ordering = ('created_at',)
