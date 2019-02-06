4c2e9fa58f1b9b4ada0bedd9f187c43603cdafd4
Julia <julia.anholcer@interia.pl>
2019-02-06 17:30:04 +0100

    PAH-149 | add alert after adding a drive (#162)
    * PAH-149 | add alert after adding a drive
    
    * PAH-149 | review requested changes

--- frontend/src/views/RouteFormView.vue
+++ frontend/src/views/RouteFormView.vue
@@ -4,6 +4,22 @@
       <div class="row">
         <div class="col-lg-8 offset-lg-2">
           <div>
+            <b-alert
+              variant="success"
+              dismissible
+              :show="confirmationOnline"
+              @dismissed="confirmationOnline=false"
+            >
+              <b>{{ $t('routes.drive_added_online_notification') }}</b>
+            </b-alert>
+            <b-alert
+              variant="secondary"
+              dismissible
+              :show="confirmationOffline"
+              @dismissed="confirmationffline=false"
+            >
+              <b>{{ $t('routes.drive_added_offline_notification') }}</b>
+            </b-alert>
             <div
               class="alert alert-danger errors"
               v-if="Object.keys(errors).length">
@@ -135,10 +151,10 @@
 <script>
 import { MultiSelect } from 'vue-search-select';
 
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapGetters, mapState } from 'vuex';
 import * as actions from '../store/actions';
 import { isErroring, makeErrors, stringFields } from './services';
-import { namespaces, actions as apiActions } from '../store/constants';
+import { namespaces, actions as apiActions, IS_ONLINE } from '../store/constants';
 
 const defaultFormState = {
   date: '',
@@ -163,6 +179,8 @@ export default {
       searchText: '',
       selectedPassengers: [],
       lastSelectPassenger: {},
+      confirmationOnline: false,
+      confirmationOffline: false,
     };
   },
   methods: {
@@ -176,11 +194,18 @@ export default {
     },
     handleSubmit() {
       this.validateForm();
+      this.confirmationOffline = false;
+      this.confirmationOnline = false;
 
       if (!Object.keys(this.errors).length) {
         this[actions.SUBMIT]({ form: { ...this.route, syncId: Math.floor(Date.now() / 1000) } });
         this.route = { ...defaultFormState };
         this.selectedPassengers = [];
+        if (this.isOnline) {
+          this.confirmationOnline = true;
+        } else {
+          this.confirmationOffline = true;
+        }
       }
     },
 
@@ -201,7 +226,7 @@ export default {
         .reduce(makeErrorsPartial, {});
 
       if (!data.passengers || !data.passengers.length) {
-        this.errors.passengers = this.$t('routes.passengers-error');
+        this.errors.passengers = this.$t('routes.passengers_error');
       }
 
       const { startMileage, endMileage } = data;
@@ -230,6 +255,7 @@ export default {
         text: [p.firstName, p.lastName].join(' '),
       })),
     }),
+    ...mapGetters([IS_ONLINE]),
     distance() {
       const distance = this.route.endMileage - this.route.startMileage;
       return distance > 0 ? distance : 0;
