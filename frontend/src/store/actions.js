a5230b9233de0093e57db48ffeae1111494ab944
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-08-11 10:35:40 +0200

    Fix login action

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,12 +1,17 @@
 import { get, patch, post } from '../services/api/http';
 import { login, saveToken, deleteToken } from '../services/api/auth';
-import { successfulLogoutRoute } from '../router/index';
 import { getMyself } from '../services/api/user';
 import * as mutations from './mutations';
 import { mapDrive } from './helpers';
 import { i18n } from '../main';
-import { actions as apiActions, namespaces, SYNC, SYNC_ITEM_SUCCESS, SYNC_ITEM_FAILURE,
-  UNSYNCHRONISED_DRIVES } from './constants';
+import {
+  actions as apiActions,
+  namespaces,
+  SYNC,
+  SYNC_ITEM_SUCCESS,
+  SYNC_ITEM_FAILURE,
+  UNSYNCHRONISED_DRIVES
+} from './constants';
 
 export const FETCH_USER = 'FETCH_USER';
 export const LOGIN = 'LOGIN';
@@ -18,7 +23,7 @@ export const SUBMIT_CONFIRMATION_TOKEN = 'SUBMIT_CONFIRMATION_TOKEN';
 
 export const actions = {
   [FETCH_USER]({ dispatch, commit }, { callback } = {}) {
-    getMyself().then((user) => {
+    getMyself().then(user => {
       commit(mutations.SET_USER, user);
       dispatch(`${namespaces.passengers}/${apiActions.fetchPassengers}`);
       dispatch(`${namespaces.cars}/${apiActions.fetchCars}`);
@@ -33,10 +38,12 @@ export const actions = {
   [LOGIN]({ commit, dispatch }, { username, password }) {
     commit(mutations.SET_LOGIN_PROGRESS, true);
     login(username, password)
-      .then((token) => {
+      .then(token => {
         commit(mutations.SET_LOGIN_ERROR, null);
         saveToken(token);
-        dispatch(FETCH_USER, { callback: () => window.location.replace('/drive') });
+        dispatch(FETCH_USER, {
+          callback: () => window.location.replace('/drive')
+        });
       })
       .catch(() => {
         commit(mutations.SET_LOGIN_ERROR, i18n.tc('login.login_error'));
@@ -49,7 +56,7 @@ export const actions = {
   [LOGOUT]({ commit }) {
     commit(mutations.SET_USER, null);
     deleteToken();
-    window.location.replace(successfulLogoutRoute.path);
+    window.location.replace('/logout_success');
   },
 
   [SUBMIT]({ commit, dispatch }, { form }) {
@@ -64,20 +71,29 @@ export const actions = {
 
   [VERIFY_CONFIRMATION_TOKEN]({ commit }, token) {
     get(`verification-token/${token}`)
-      .catch((err) => {
-        commit(mutations.SET_VERIFICATION_TOKEN_ACTIVE, { token, isActive: false });
+      .catch(err => {
+        commit(mutations.SET_VERIFICATION_TOKEN_ACTIVE, {
+          token,
+          isActive: false
+        });
         throw err;
       })
       .then(resp => resp.isActive)
-      .then(isActive => commit(mutations.SET_VERIFICATION_TOKEN_ACTIVE, { token, isActive }))
+      .then(isActive =>
+        commit(mutations.SET_VERIFICATION_TOKEN_ACTIVE, { token, isActive })
+      )
       .catch(() => console.debug(`Token ${token} not found.`));
   },
 
   [SUBMIT_CONFIRMATION_TOKEN]({ commit }, { payload, token }) {
     return patch(`verification-token/${token}`, payload, false)
       .then(resp => resp.isActive)
-      .then(isActive => commit(mutations.SET_VERIFICATION_TOKEN_ACTIVE, { token, isActive }))
-      .then(() => commit(mutations.SET_VERIFICATION_TOKEN_SUBMISSION_PROGRESS, false));
+      .then(isActive =>
+        commit(mutations.SET_VERIFICATION_TOKEN_ACTIVE, { token, isActive })
+      )
+      .then(() =>
+        commit(mutations.SET_VERIFICATION_TOKEN_SUBMISSION_PROGRESS, false)
+      );
   },
 
   async [SYNC]({ dispatch, state, commit }) {
@@ -86,7 +102,11 @@ export const actions = {
       return;
     }
 
-    if (state[UNSYNCHRONISED_DRIVES].length === 0 || !state.user || !navigator.onLine) {
+    if (
+      state[UNSYNCHRONISED_DRIVES].length === 0 ||
+      !state.user ||
+      !navigator.onLine
+    ) {
       return;
     }
 
@@ -105,5 +125,5 @@ export const actions = {
       }
     }
     dispatch(SYNC);
-  },
+  }
 };
