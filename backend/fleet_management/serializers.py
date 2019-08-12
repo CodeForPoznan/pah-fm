9d09c0f31edd08c1855e70319fd57155353a14d1
Adam Gajzlerowicz <nelf86@gmail.com>
2019-08-12 13:26:21 +0200

    Linter (#306)
    Udated linter to flake8

--- backend/fleet_management/serializers.py
+++ backend/fleet_management/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import fields, serializers, status
 from rest_framework.exceptions import ValidationError
 
 from .models import Car, Drive, Passenger, User, Project, VerificationToken
-from .signals import drive_created
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -82,18 +81,6 @@ class DriveSerializer(serializers.ModelSerializer):
             drive.passengers.set(passengers)
             drive.save()
 
-            for passenger in passengers:
-                token = VerificationToken.objects.create(
-                    drive=drive,
-                    passenger=passenger,
-                )
-                drive_created.send(
-                    self.__class__,
-                    drive_id=drive.id,
-                    driver_id=self.context['driver'].id,
-                    passenger_id=passenger.id,
-                    token_id=token.id,
-                )
             return drive
 
     def is_valid(self, raise_exception=False):
