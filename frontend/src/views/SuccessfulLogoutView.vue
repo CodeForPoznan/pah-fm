641843f3001eee9a4e3ac490b2325ad79b6d19f7
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-09-01 10:39:48 +0200

    Add margin

--- frontend/src/views/SuccessfulLogoutView.vue
+++ frontend/src/views/SuccessfulLogoutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>{{ $t('logout.thank_you') }}</h1>
     <p>{{ $t('logout.successfully_logged_out') }}</p>
     <a href="/login">{{ $t('logout.login_again') }}</a>
@@ -11,3 +11,9 @@ export default {
   name: 'SuccessfulLogoutView',
 };
 </script>
+
+<style scoped>
+.container {
+  margin: 0 15px;
+}
+</style>
