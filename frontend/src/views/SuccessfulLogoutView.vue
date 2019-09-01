3230b5a444ae8975f2be1597ffdfdc7c98274b1e
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-09-01 10:59:08 +0200

    Remove unnecessary margin

--- frontend/src/views/SuccessfulLogoutView.vue
+++ frontend/src/views/SuccessfulLogoutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div>
     <h1>{{ $t('logout.thank_you') }}</h1>
     <p>{{ $t('logout.successfully_logged_out') }}</p>
     <a href="/login">{{ $t('logout.login_again') }}</a>
@@ -11,9 +11,3 @@ export default {
   name: 'SuccessfulLogoutView',
 };
 </script>
-
-<style scoped>
-.container {
-  margin: 0 15px;
-}
-</style>
