775f99bbace5dfe0f48b77bc95c19808651574f6
Alan Sobócki <ctilhs@gmail.com>
2019-10-08 20:18:58 +0200

    Add changes based on code review which don't require offline discussion.

--- backend/fleet_management/tests/test_drives.py
+++ backend/fleet_management/tests/test_drives.py
@@ -1,7 +1,6 @@
 from datetime import date
 
 from django.contrib.auth.models import Group
-from django.test import override_settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -24,18 +23,34 @@ class DrivesApiTest(APITestCase):
         self.car = CarFactory()
         self.project = ProjectFactory(country="UA")
         self.driver = UserFactory()
-        self.driver.groups.set(Group.objects.filter(name=Groups.Driver.name))
+        self.driver_group = Group.objects.filter(name=Groups.Driver.name)
+        self.driver.groups.set(self.driver_group)
 
         self.drives = DriveFactory.create_batch(
             size=10,
-            car=self.car,
             driver=self.driver,
-            date=date.today().isoformat(),
-            project=self.project,
+            passengers=self.passengers,
         )
 
-        self.drives[0].passengers.set(self.passengers)
+    def test_401_for_unlogged_user(self):
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_can_retrieve_only_my_drives(self):
+        new_driver = UserFactory()
+        new_driver.groups.set(self.driver_group)
+        my_drive_id = DriveFactory(driver=new_driver).id
+
+        self.client.force_login(new_driver)
+        res = self.client.get(self.url)
+        drives = res.data
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(drives), 1)
+        self.assertEqual(drives[0]["id"], my_drive_id)
+
+
+    def test_can_create_a_drive(self):
         self.payload = {
             "car": {"id": self.car.id},
             "passengers": [
@@ -50,30 +65,6 @@ class DrivesApiTest(APITestCase):
             "endLocation": "Poznan",
             "project": {"id": self.project.id},
         }
-
-    def test_401_for_unlogged_user(self):
-        res = self.client.get(self.url)
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_can_retrieve_only_my_drives(self):
-        new_driver = UserFactory()
-        new_driver.groups.set(Group.objects.filter(name=Groups.Driver.name))
-        new_drive = [
-            DriveFactory(
-                car=self.car, driver=new_driver, date=date.today(), project=self.project
-            )
-        ]
-
-        self.client.force_login(new_driver)
-        res = self.client.get(self.url)
-        drives = res.data
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(drives), 1)
-        self.assertEqual(drives[0]["id"], new_drive[0].id)
-
-    @override_settings(EMAIL_BACKEND="django.core.mail.backends.filebased.EmailBackend")
-    def test_can_create_a_drive(self):
-        """This test allows to verify email template."""
         self.client.force_login(self.driver)
         res = self.client.post(self.url, data=self.payload, format="json")
         drive = Drive.objects.get(pk=res.data["id"])
