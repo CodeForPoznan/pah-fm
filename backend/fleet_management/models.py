3f7eac352e43a053247e3ad0125b9467fee61c85
Ctilhs <ctilhs@gmail.com>
2019-10-23 21:45:53 +0200

    Add rsa fields to User model. (#360)
    * Add rsa fields to User model.
    
    * Add migration.
    
    * Fixed build error. Took solution from https://github.com/CodeForPoznan/pah-fm/commit/819c7354340434c5e38578dce5083ba7b76d359f
    
    * Reorder migration after recent changes in develop.

--- backend/fleet_management/models.py
+++ backend/fleet_management/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 from django_countries.fields import CountryField
-
+from django.core.validators import MinLengthValidator
 
 def get_current_timestamp_in_gmt():
     return calendar.timegm(time.gmtime())
@@ -13,6 +13,9 @@ def get_current_timestamp_in_gmt():
 
 class User(AbstractUser):
     country = CountryField(blank_label="(select country)", null=False)
+    rsa_modulus_n = models.CharField(max_length=6, validators=[MinLengthValidator(6)], null=False, default='')
+    rsa_pub_e = models.CharField(max_length=6, validators=[MinLengthValidator(6)], null=False, default='')
+    rsa_priv_d = models.CharField(max_length=6, validators=[MinLengthValidator(6)], null=False, default='')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
