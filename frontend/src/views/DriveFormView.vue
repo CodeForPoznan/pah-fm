41dd356b2d61e9a6057d7a6d367b5c4c2e3e3629
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-11-26 22:29:40 +0100

    Fix linting errors

--- frontend/src/views/DriveFormView.vue
+++ frontend/src/views/DriveFormView.vue
@@ -4,7 +4,9 @@
       <div class="row">
         <div class="col-lg-8 offset-lg-2">
           <div>
-            <div class="alert alert-danger errors" v-if="Object.keys(errors).length">
+            <div
+              class="alert alert-danger errors"
+              v-if="Object.keys(errors).length">
               <b>{{ $t('drive_form.please_correct_errors') }}</b>
               <ul class="error-list">
                 <li
@@ -26,7 +28,7 @@
                   :max="currentDate"
                   class="form-control"
                   :class="{ 'is-invalid': errors['date'] }"
-                />
+                >
               </div>
 
               <div class="form-group">
@@ -39,7 +41,7 @@
                   maxlength="100"
                   class="form-control"
                   :class="{ 'is-invalid': errors['startLocation'] }"
-                />
+                >
               </div>
               <div class="form-group">
                 <label>{{ $t('drive_form.starting_mileage') }}</label>
@@ -55,7 +57,7 @@
                   @input="syncToLocalStorage"
                   class="form-control"
                   :class="{ 'is-invalid': errors['startMileage'] }"
-                />
+                >
               </div>
               <div class="form-group">
                 <label>{{ $t('drive_form.project') }}</label>
@@ -89,9 +91,14 @@
                   class="form-control"
                   :class="{ 'is-invalid': errors['car'] }"
                 >
-                  <option v-for="car in cars.data" :key="car.id" :value="car.id">{{ car.plates }}</option>
+                  <option
+                    v-for="car in cars.data"
+                    :key="car.id"
+                    :value="car.id">{{ car.plates }}</option>
                 </select>
-                <p class="font-weight-bold" v-if="!cars.data">{{ $t('drive_form.no_cars_message') }}</p>
+                <p
+                  class="font-weight-bold"
+                  v-if="!cars.data">{{ $t('drive_form.no_cars_message') }}</p>
               </div>
 
               <div class="form-group">
@@ -117,7 +124,7 @@
                   name="description"
                   class="form-control"
                   :class="{ 'is-invalid': errors['description']}"
-                />
+                >
               </div>
 
               <div class="form-group">
@@ -130,7 +137,7 @@
                   name="endLocation"
                   class="form-control"
                   :class="{ 'is-invalid': errors['endLocation'] }"
-                />
+                >
               </div>
               <div class="form-group">
                 <label>{{ $t('drive_form.ending_mileage') }}</label>
@@ -146,7 +153,7 @@
                   name="endMileage"
                   class="form-control"
                   :class="{ 'is-invalid': errors['endMileage'] }"
-                />
+                >
               </div>
               <div class="form-group">
                 <label for="driveHash">{{ $t('drive_form.drive_hash') }}</label>
@@ -156,10 +163,10 @@
                   class="form-control"
                   type="text"
                   readonly
-                />
+                >
               </div>
               <div class="form-group">
-                <label for="confirmHash">{{$t('drive_form.confirm_hash')}}</label>
+                <label for="confirmHash">{{ $t('drive_form.confirm_hash') }}</label>
                 <input
                   type="text"
                   id="confirmHash"
@@ -169,7 +176,7 @@
                   maxlength="6"
                   class="form-control"
                   :class="{ 'is-invalid': errors['endMileage'] }"
-                />
+                >
               </div>
               <div
                 class="form-group col-xs-12"
@@ -281,7 +288,7 @@ export default {
           ...acc,
           [key]: stringFields.includes(key) ? String(value).trim() : value,
         }),
-        {}
+        {},
       );
 
       this.errors = Object.keys(data)
@@ -307,14 +314,14 @@ export default {
 
   computed: {
     ...mapState(namespaces.cars, {
-      cars: (state) => state,
+      cars: state => state,
     }),
     ...mapState(namespaces.projects, {
-      projects: (state) => state,
+      projects: state => state,
     }),
     ...mapState(namespaces.passengers, {
-      passengers: (state) =>
-        (state.data || []).map((p) => ({
+      passengers: state =>
+        (state.data || []).map(p => ({
           value: p.id,
           text: [p.firstName, p.lastName].join(' '),
         })),
