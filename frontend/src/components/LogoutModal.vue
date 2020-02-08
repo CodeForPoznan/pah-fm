473a3e0f5542598b0e21c52d3d25ea4ba941b61a
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-08 18:43:11 +0100

    Add LogoutModal

---
+++ frontend/src/components/LogoutModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-modal
+    id="logout-modal"
+    title="Are you sure?"
+    @ok="logout"
+  >
+    <p>
+      You are offline so if you log out you will no able to log in again until
+      you will be online again
+    </p>
+  </b-modal>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+import { LOGOUT } from '../store/actions';
+
+export default {
+  name: 'LogoutModal',
+  methods: {
+    ...mapActions([LOGOUT]),
+  },
+};
+</script>
