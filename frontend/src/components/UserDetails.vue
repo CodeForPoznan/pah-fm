bd4d2b8adf69746cba94d290206fd7391017429d
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-08 18:05:41 +0100

    Add UserDetails

---
+++ frontend/src/components/UserDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-nav-item
+    v-if="user"
+    @click="LOGOUT"
+    to="/logout"
+    key="logout"
+    class="username"
+    :class="{ offline: !isOnline }"
+  >
+    {{ $t('common.logout') }}
+    <p>{{ user.username }}</p>
+  </b-nav-item>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from 'vuex';
+
+import { USER } from '../store';
+import { LOGOUT } from '../store/actions';
+import { IS_ONLINE } from '../store/constants';
+
+export default {
+  name: 'UserDetails',
+  computed: {
+    ...mapState([USER]),
+    ...mapGetters([IS_ONLINE]),
+  },
+  methods: {
+    ...mapActions([LOGOUT]),
+  },
+};
+</script>
+
+<style scoped>
+.offline {
+  cursor: default;
+  pointer-events: none;
+  text-decoration: none;
+}
+</style>
