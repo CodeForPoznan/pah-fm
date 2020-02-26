7d85b2124513cec79bdd8ef6df58db1461ebf726
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-26 18:56:23 +0100

    Add pop-up for online users

--- frontend/src/components/LogoutModal.vue
+++ frontend/src/components/LogoutModal.vue
@@ -8,18 +8,27 @@
     @ok="logout"
     class="logout-modal"
   >
-    <p>
+    <p v-if="isOnline">
+      {{ $t('logout.logout_warning') }}
+    </p>
+    <p v-else>
       {{ $t('logout.offline_logout_warning') }}
     </p>
   </b-modal>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 import router from '../router';
 import { logoutRoute } from '../router/routes';
+import { IS_ONLINE } from '../store/constants';
 
 export default {
   name: 'LogoutModal',
+  computed: {
+    ...mapState([IS_ONLINE]),
+  },
   methods: {
     logout() {
       router.push(logoutRoute);
--- frontend/src/components/UserDetails.vue
+++ frontend/src/components/UserDetails.vue
@@ -3,24 +3,22 @@
     <b-nav-item
       v-if="user"
       v-b-modal.logout-modal
-      @click="logout"
       key="logout"
       class="username"
     >
       {{ $t('common.logout') }}
       <p>{{ user.username }}</p>
     </b-nav-item>
-    <logout-modal v-if="!isOnline" />
+    <logout-modal />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
+import { mapState } from 'vuex';
 
 import router from '../router';
 import { logoutRoute } from '../router/routes';
 import { USER } from '../store';
-import { IS_ONLINE } from '../store/constants';
 
 import LogoutModal from './LogoutModal.vue';
 
@@ -31,14 +29,6 @@ export default {
   },
   computed: {
     ...mapState([USER]),
-    ...mapGetters([IS_ONLINE]),
-  },
-  methods: {
-    logout() {
-      if (this.isOnline) {
-        router.push(logoutRoute);
-      }
-    },
   },
 };
 </script>
