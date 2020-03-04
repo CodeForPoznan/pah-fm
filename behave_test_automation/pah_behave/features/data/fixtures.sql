fe3fe71941abd25178de3b08c19db20e37976c33
Ctilhs <ctilhs@gmail.com>
2020-03-04 22:15:11 +0100

    442 Remove redundant file, description and makefile command.

--- behave_test_automation/pah_behave/features/data/fixtures.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.fleet_management_user(
-    password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined, country, rsa_modulus_n, rsa_priv_d, rsa_pub_e)
-    VALUES ('pbkdf2_sha256$120000$B4VUk7IZMZCk$t9nZBMo6q4EghFgeqkTLhstGRTlYCRfHJtGkuNzUTK0=', FALSE, 'solomiiavyshniak@eukr.net', 'Ада', 'Гавриленко', 'solomiiavyshniak@ukr.net', FALSE, TRUE, '2019-02-18', 'UA', '55555', '33333', '222');
