86f76d6c9869ca2ad94e1c9c4eb2a1392c1d0efe
Artur Tamborski <tamborskiartur@gmail.com>
2020-03-19 14:46:45 +0100

    [448] Move Admin classes back to Admin, remove unneded filters in CustomUserAdmin, introduce dynamic CountryFilter

--- backend/fleet_management/admin.py
+++ backend/fleet_management/admin.py
@@ -1,27 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
+
 from import_export.admin import ImportExportModelAdmin
 from import_export.fields import Field
 from import_export import resources
-from .models import Car, Drive, User, Project, ProjectAdmin, CarAdmin
+
+from fleet_management.models import Car, Drive, User, Project
+
+
+class CountryFilter(admin.SimpleListFilter):
+    title = _("Country")
+    parameter_name = "country"
+
+    def lookups(self, request, model_admin):
+        objects = model_admin.model.objects.distinct(self.parameter_name)
+        countries = [(o.country.code, o.country.name) for o in objects]
+        countries = sorted(countries, key=lambda c: c[1])  # sort by name, A-Z
+        return [("ALL", _("Global"))] + countries
+
+    def queryset(self, request, queryset):
+        value = self.value()
+
+        # "ALL" is special value used for showing global users (with empty country)
+        if value == "ALL":
+            value = ""
+
+        if value is not None:
+            return queryset.filter(**{self.parameter_name: value})
+
+        return queryset
+
+
+class DriveCountryFilter(CountryFilter):
+    parameter_name = "driver__country"
 
 
 class DriveResource(resources.ModelResource):
-
-    def dehydrate_driver(self, drive):
-        return str(drive.driver)
-
-    def dehydrate_driver__country(self, drive):
-        return str(drive.driver.country.name)
-
-    def dehydrate_passenger(self, drive):
-        return str(drive.passenger)
-
-    # patch model properties
-    fuel_consumption = Field(attribute="fuel_consumption")
-    diff_mileage = Field(attribute="diff_mileage")
     country = Field(attribute="country")
+    diff_mileage = Field(attribute="diff_mileage")
+    fuel_consumption = Field(attribute="fuel_consumption")
 
     class Meta:
         model = Drive
@@ -44,14 +62,43 @@ class DriveResource(resources.ModelResource):
         )
         export_order = fields
 
+    def dehydrate_driver(self, drive):
+        return str(drive.driver)
 
+    def dehydrate_driver__country(self, drive):
+        return str(drive.driver.country.name)
+
+    def dehydrate_passenger(self, drive):
+        return str(drive.passenger)
+
+
+@admin.register(Drive)
 class DriveAdmin(ImportExportModelAdmin):
     resource_class = DriveResource
-    list_filter = ("driver__country",)
-    list_display = ("__str__", "country", "is_verified")
+    list_filter = (DriveCountryFilter,)
+    list_display = ("__str__", "country__name", "is_verified")
+
+    def country__name(self, drive):
+        return drive.country.name
 
 
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_filter = (CountryFilter,)
+    list_display = ("__str__", "country")
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_filter = (CountryFilter,)
+    list_display = ("__str__", "country")
+
+
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    list_filter = ("groups", CountryFilter)
+    list_display = ("username", "first_name", "last_name", "country", "is_staff")
+
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         (
@@ -72,10 +119,3 @@ class CustomUserAdmin(UserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'country',)
-
-
-admin.site.register(Car, CarAdmin)
-admin.site.register(Drive, DriveAdmin)
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(Project, ProjectAdmin)
