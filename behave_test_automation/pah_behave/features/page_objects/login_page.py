094dc324af9f9b60d82ce61e08ee167ba15046dc
kruczynakruka <victoria.kruczek@stxnext.pl>
2020-03-22 19:59:13 +0100

    Added scenarios with language change

--- behave_test_automation/pah_behave/features/page_objects/login_page.py
+++ behave_test_automation/pah_behave/features/page_objects/login_page.py
@@ -10,6 +10,21 @@ class LoginPage(BasePage):
     login_failed_message = (By.CSS_SELECTOR, '.alert-danger')
     hamburger_menu = (By.CSS_SELECTOR, '.bm-burger-button')
 
+    def change_language_selector(self, language):
+        return By.CSS_SELECTOR, f'span[title="{language}"]'
+
+    def login_text_selector(self, login_translation):
+        return By.XPATH, f'//h2[contains(text(), "{login_translation}")]'
+
+    def username_text_selector(self, username_translation):
+        return By.XPATH, f'//label[contains(text(), "{username_translation}")]'
+
+    def password_text_selector(self, password_translation):
+        return By.XPATH, f'//label[contains(text(), "{password_translation}")]'
+
+    def button_text_selector(self, login_button_translation):
+        return By.XPATH, f'//button[contains(text(), "{login_button_translation}")]'
+
     def submit_login_form(self, login, password):
         self.find_element(*self.username_field).send_keys(login)
         self.find_element(*self.password_field).send_keys(password)
@@ -31,3 +46,21 @@ class LoginPage(BasePage):
         self.find_element(*self.password_field).send_keys("cfp123")
         self.find_element(*self.login_button_enabled).click()
         self.wait_for_element(self.hamburger_menu)
+
+    def change_language(self, language):
+        self.find_element(*self.change_language_selector(language)).click()
+
+    def translation_login_view(self, login_title, username, password, login_button):
+        self.find_element(*self.login_text_selector(login_title))
+        self.find_element(*self.username_text_selector(username))
+        self.find_element(*self.password_text_selector(password))
+        self.find_element(*self.button_text_selector(login_button))
+
+    def input_valid_credentials_to_login_form(self):
+        self.delete_all_cookies()
+        self.find_element(*self.username_field).send_keys("hello@codeforpoznan.pl")
+        self.find_element(*self.password_field).send_keys("cfp123")
+
+    def switch_language_and_submit(self, language):
+        self.find_element(*self.change_language_selector(language)).click()
+        self.find_element(*self.login_button_enabled).click()
