fe903b161f42b5f68893056a8748bdc8973fd635
kruczyna <31814183+kruczyna@users.noreply.github.com>
2020-03-24 11:42:39 +0100

    Login again via logout view (#473)
    * Added country value to fixture - fix
    
    * Added country value to fixture - fix
    
    * Changed country value from EN to UA
    
    * Added User logs in after logout via logout view scenario
    
    Co-authored-by: Victoria Kruczek <victoria.kruczek@lerta.energy>
    Co-authored-by: kruczynakruka <victoria.kruczek@stxnext.pl>

--- behave_test_automation/pah_behave/features/page_objects/logout_page.py
+++ behave_test_automation/pah_behave/features/page_objects/logout_page.py
@@ -7,6 +7,7 @@ class LogoutPage(BasePage):
     logout_button = (By.CSS_SELECTOR, 'a[href="/logout"]')
     logout_message = (By.CSS_SELECTOR, '.logout')
     hamburger_menu = (By.CSS_SELECTOR, 'div.bm-burger-button')
+    login_link = (By.CSS_SELECTOR, 'a[href="/login"]')
 
     def logout_via_logout_button(self):
         self.wait_for_element(self.hamburger_menu)
@@ -16,9 +17,13 @@ class LogoutPage(BasePage):
         self.wait_for_element(self.logout_button)
         logout_button = self.find_element(*self.logout_button)
         self.browser.execute_script("arguments[0].click();", logout_button)
+        self.wait_for_element(self.login_link)
 
     def logged_out_user_state(self):
         self.wait_for_element(self.logout_message)
 
     def enter_logout_url(self):
         self.visit()
+
+    def navigate_to_login_via_logout(self):
+        self.find_element(*self.login_link).click()
