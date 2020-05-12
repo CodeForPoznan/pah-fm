2c7e4f59b6df65e630ff618bedcf1ef74fe57296
Artur Tamborski <tamborskiartur@gmail.com>
2020-05-12 17:35:51 +0200

    [226] Prepare application for serverless setup

--- backend/handlers.py
+++ backend/handlers.py
@@ -4,9 +4,18 @@ import os
 import django
 from django.core.management import call_command
 
+from serverless_wsgi import handle_request
+
+from pah_fm.wsgi import application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pah_fm.settings')
 django.setup()
+
+
+def api(event, context):
+    # event["headers"]["X-Forwarded-Host"] = "dev.pahfm.codeforpoznan.pl"
+    return handle_request(application, event, context)
 
 
 def migration(event, context):
