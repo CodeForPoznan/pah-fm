087030f37827e914d2fce6bc5b55499f503919cb
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-05-20 21:05:02 +0200

    Remove AUTH_HEADER
    It wasn't used

--- frontend/src/store/modules/http/getters.js
+++ frontend/src/store/modules/http/getters.js
@@ -1,19 +1,18 @@
 import { TOKEN } from './state';
 
-export const AUTH_HEADER = 'AUTH_HEADER';
 export const AUTH_DATA = 'AUTH_DATA';
 export const IS_USER_LOGGED_IN = 'IS_USER_LOGGED_IN';
 
 export default {
-  [AUTH_DATA]: (state, getters) => ({ requestOptions, auth }) => {
+  [AUTH_DATA]: (state) => ({ requestOptions, auth = true }) => {
     if (auth) {
-      const authHeader = getters[AUTH_HEADER];
-      const headers = { ...requestOptions.headers, ...authHeader };
-      return { ...requestOptions, headers };
+      if (state[TOKEN]) {
+        const headers = { ...requestOptions.headers, Authorization: `JWT ${state[TOKEN]}` };
+        return { ...requestOptions, headers };
+      }
+      return requestOptions;
     }
     return { ...requestOptions, credentials: 'omit' };
   },
-  [AUTH_HEADER]: state => (state[TOKEN] ? {
-    Authorization: `JWT ${state[TOKEN]}` } : {}),
   [IS_USER_LOGGED_IN]: state => !!state[TOKEN],
 };
--- frontend/src/tests/store/modules/http.test.js
+++ frontend/src/tests/store/modules/http.test.js
@@ -1,27 +1,46 @@
 import http from '../../../store/modules/http';
 import { TOKEN } from '../../../store/modules/http/state';
-import { AUTH_HEADER } from '../../../store/modules/http/getters';
+import { AUTH_DATA } from '../../../store/modules/http/getters';
 
 const { getters } = http;
 
 const exampleJWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImhlbGxvQGNvZGVmb3Jwb3puYW4ucGwiLCJleHAiOjE1OTE5ODIxMTYsImVtYWlsIjoiaGVsbG9AY29kZWZvcnBvem5hbi5wbCJ9.HeTCwtaVh8Ws0ZwMqVRpxJE5hdkgSrLcdlyghN9M9WI';
 
 describe('HTTP Module', () => {
-  it('AUTH_HEADER returns valid header object', () => {
+  it('AUTH_DATA returns valid options object', () => {
     let state = {
       [TOKEN]: exampleJWT,
     };
 
-    let header = getters[AUTH_HEADER](state);
-    expect(header).toStrictEqual({
+    let requestOptions = {
+      method: 'POST'
+    }
+
+    let headers = getters[AUTH_DATA](state)({ requestOptions }).headers
+    expect(headers).toStrictEqual({
       Authorization: `JWT ${exampleJWT}`,
     });
+
+    requestOptions = {
+      ...requestOptions,
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+
+    headers = getters[AUTH_DATA](state)({ requestOptions }).headers
+    expect(headers).toStrictEqual({
+      Accept: 'application/json',
+      Authorization: `JWT ${exampleJWT}`
+    })
+
+    delete requestOptions.headers
 
     state = {
       [TOKEN]: null,
     };
 
-    header = getters[AUTH_HEADER](state);
-    expect(header).toStrictEqual({});
+    headers = getters[AUTH_DATA](state)({ requestOptions }).headers
+    expect(headers).toBe(undefined);
   });
 });
