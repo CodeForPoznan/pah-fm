0406221992c0eb974d124501019e63b16e07341f
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-05-20 21:06:24 +0200

    Remove CLEAR_SESSION
    It wasn't used

--- frontend/src/store/modules/http/mutations.js
+++ frontend/src/store/modules/http/mutations.js
@@ -1,13 +1,9 @@
 import { TOKEN } from './state';
 
 export const SET_TOKEN = 'SET_TOKEN';
-export const CLEAR_SESSION = 'CLEAR_SESSION';
 
 export default {
   [SET_TOKEN]: (state, token) => {
     Object.assign(state, { [TOKEN]: token });
-  },
-  [CLEAR_SESSION]: (state) => {
-    Object.assign(state, { [TOKEN]: null });
   },
 };
