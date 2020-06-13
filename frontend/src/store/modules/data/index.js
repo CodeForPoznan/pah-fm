0d2579abe94ab325a4df438a4e28f07bfc0330a3
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-06-13 19:24:29 +0200

    Fix disappearing values in form

--- frontend/src/store/modules/data/index.js
+++ frontend/src/store/modules/data/index.js
@@ -40,6 +40,7 @@ const mutations = {
   [CLEAR_NEW_DRIVE_FORM]: state =>
     Object.assign(state, {
       [NEW_DRIVE_FORM]: {
+	...newDriveFormInitialState,
         date: getToday(),
         car: state[NEW_DRIVE_FORM].car,
       },
