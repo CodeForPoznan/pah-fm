dca47b5b574b67cd53a3532fd7e086ae8f2e4eed
bckamil <43443201+bckamil@users.noreply.github.com>
2020-06-24 20:48:40 +0200

    461 Detect when user was last time online (#490)
    * 461 Detect when user was last time online
    
    * Update middleware.py
    
    * Update middleware.py

--- backend/pah_fm/settings.py
+++ backend/pah_fm/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'fleet_management.middleware.UpdateLastSeenMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = (
