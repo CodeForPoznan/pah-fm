f5599658c2106be48e61ca2b5ef5734f0f55dbf5
Artur Tamborski <tamborskiartur@gmail.com>
2020-07-09 11:30:16 +0200

    Revert fragment on legacy support

--- frontend/src/store/modules/http/state.js
+++ frontend/src/store/modules/http/state.js
@@ -1,1 +1,7 @@
 export const TOKEN = 'TOKEN';
+
+export default {
+    // LEGACY SUPPORT
+    // After merge to new version remove localStorage
+    [TOKEN]: localStorage.getItem('jwt') || null,
+};
