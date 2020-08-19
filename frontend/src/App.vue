cc879e7ff265755f57fb1cf375c0c8193196bc84
OtisRed <durczak.krzysztof@gmail.com>
2020-08-19 23:41:01 +0200

    working semi-solution

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <Status />
-    <Header />
-    <sidebar :show="showMenu && IS_USER_LOGGED_IN" />
-    <div
-      id="page-wrap"
-      class="container"
-    >
+    <div class="container">
       <div class="row">
-        <div class="col-xs-12 page">
-          <transition
-            name="fade"
-            mode="out-in"
-            appear
-          >
-            <router-view @hide-menu="showMenu = false" />
-          </transition>
+        <div class="col-6 p-0">
+          <Header />
+        </div>
+        <div class="col-3">
+          <Status />
+        </div>
+        <div class="col-3">
+          <sidebar :show="showMenu && IS_USER_LOGGED_IN" />
+        </div>
+      </div>
+      <div
+              id="page-wrap"
+              class="container"
+      >
+        <div class="row">
+          <div class="col-xs-12 page">
+            <transition
+                    name="fade"
+                    mode="out-in"
+                    appear
+            >
+              <router-view @hide-menu="showMenu = false" />
+            </transition>
+          </div>
         </div>
       </div>
     </div>
@@ -24,113 +34,129 @@
 </template>
 
 <script>
-import { mapState, mapActions, mapGetters } from 'vuex';
+  import { mapState, mapActions, mapGetters } from 'vuex';
 
-import Header from './components/Header.vue';
-import Refresh from './components/Refresh.vue';
-import Status from './components/Status.vue';
-import Sidebar from './components/Sidebar.vue';
-import store, { LANGUAGE } from './store';
+  import Header from './components/Header.vue';
+  import Refresh from './components/Refresh.vue';
+  import Status from './components/Status.vue';
+  import Sidebar from './components/Sidebar.vue';
+  import store, { LANGUAGE } from './store';
 
-import { IS_USER_LOGGED_IN } from './store/modules/http/getters';
+  import { IS_USER_LOGGED_IN } from './store/modules/http/getters';
 
-import { SYNC } from './store/constants';
-import { FETCH_USER } from './store/actions';
+  import { SYNC } from './store/constants';
+  import { FETCH_USER } from './store/actions';
 
-export default {
-  name: 'App',
-  store,
-  components: {
-    Refresh,
-    Header,
-    Status,
-    Sidebar,
-  },
-  data() {
-    return {
-      showMenu: true,
-    };
-  },
-  computed: {
-    ...mapState([LANGUAGE]),
-    ...mapGetters('http', [IS_USER_LOGGED_IN]),
-  },
-  methods: {
-    ...mapActions({
-      sync: SYNC,
-      fetchUser: FETCH_USER,
-    }),
-  },
-  created() {
-    if (this.language) {
-      /* eslint-disable-next-line no-underscore-dangle */
-      this._i18n.locale = this.language;
-    }
-    if (this[IS_USER_LOGGED_IN]) {
-      this.sync();
-      this.fetchUser();
-    }
-  },
-};
+  export default {
+    name: 'App',
+    store,
+    components: {
+      Refresh,
+      Header,
+      Status,
+      Sidebar,
+    },
+    data() {
+      return {
+        showMenu: true,
+      };
+    },
+    computed: {
+      ...mapState([LANGUAGE]),
+      ...mapGetters('http', [IS_USER_LOGGED_IN]),
+    },
+    methods: {
+      ...mapActions({
+        sync: SYNC,
+        fetchUser: FETCH_USER,
+      }),
+    },
+    created() {
+      if (this.language) {
+        /* eslint-disable-next-line no-underscore-dangle */
+        this._i18n.locale = this.language;
+      }
+      if (this[IS_USER_LOGGED_IN]) {
+        this.sync();
+        this.fetchUser();
+      }
+    },
+  };
 </script>
 
 <style lang="scss">
-@import './scss/base';
+  @import './scss/base';
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background: $white;
-  min-height: 100vh;
-}
-
-.bm-item-list {
-  margin-right: 10%;
-  margin-left: 10%;
-}
-
-.nav {
-  flex-wrap: nowrap;
-}
-
-.mobile-menu {
-  .bm-menu {
-    background: $pah-color-3;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background: $white;
+    min-height: 100vh;
   }
 
-  .bm-cross {
-    height: 30px !important;
+  .bm-item-list {
+    margin-right: 10%;
+    margin-left: 10%;
   }
 
-  .cross-style {
-    top: 36px !important;
-    right: 36px !important;
+  .nav {
+    flex-wrap: nowrap;
   }
 
-  & .nav-item a {
-    color: $white;
+  .mobile-menu {
+    .bm-menu {
+      background: $pah-color-3;
+    }
+
+    .bm-burger-button {
+      position: fixed;
+      width: 36px;
+      height: 30px;
+      left: 36px;
+      top: 44px;
+      cursor: pointer;
+    }
+
+    .bm-cross {
+      height: 30px !important;
+    }
+
+    .cross-style {
+      top: 36px !important;
+      right: 36px !important;
+    }
+
+    & .nav-item a {
+      color: $white;
+    }
+
+    & .nav-item a:hover {
+      text-decoration: underline;
+    }
+
+    & .nav {
+      flex-direction: column;
+    }
+
+    & .username {
+      font-size: 0.75em;
+    }
   }
 
-  & .nav-item a:hover {
-    text-decoration: underline;
+  .is-invalid {
+    border-color: red !important;
   }
 
-  & .nav {
-    flex-direction: column;
+  .page {
+    width: 100%;
   }
 
-  & .username {
-    font-size: 0.75em;
+  .row {
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    min-height: 15vh;
   }
-}
-
-.is-invalid {
-  border-color: red !important;
-}
-
-.page {
-  width: 100%;
-}
 </style>
--- frontend/src/components/Status.vue
+++ frontend/src/components/Status.vue
@@ -1,52 +1,64 @@
 <template>
-  <div
-    v-if="!isLogin && user"
-    class="is-online"
-    :class="{ online: isOnline }"
-  />
+  <div v-if="!isLogin && user">
+    <div v-if="isOnline" class="online" @click="">
+      <svg @click="triggerSync" width="2.5em" height="2.5em" viewBox="0 0 16 16" fill="#000000" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
+        <path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
+      </svg>
+    </div>
+    <div v-else class="offline">
+      <svg width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-wifi-off" fill="#ff0000" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.706 3.294A12.545 12.545 0 0 0 8 3 12.44 12.44 0 0 0 .663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c.63 0 1.249.05 1.852.148l.854-.854zM8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065 8.448 8.448 0 0 1 3.51-1.27L8 6zm2.596 1.404l.785-.785c.63.24 1.228.545 1.785.907a.482.482 0 0 1 .063.745.525.525 0 0 1-.652.065 8.462 8.462 0 0 0-1.98-.932zM8 10l.934-.933a6.454 6.454 0 0 1 2.012.637c.285.145.326.524.1.75l-.015.015a.532.532 0 0 1-.611.09A5.478 5.478 0 0 0 8 10zm4.905-4.905l.747-.747c.59.3 1.153.645 1.685 1.03a.485.485 0 0 1 .048.737.518.518 0 0 1-.668.05 11.496 11.496 0 0 0-1.812-1.07zM9.02 11.78c.238.14.236.464.04.66l-.706.706a.5.5 0 0 1-.708 0l-.707-.707c-.195-.195-.197-.518.04-.66A1.99 1.99 0 0 1 8 11.5c.373 0 .722.102 1.02.28zm4.355-9.905a.53.53 0 1 1 .75.75l-10.75 10.75a.53.53 0 0 1-.75-.75l10.75-10.75z"/>
+      </svg>
+      <p>offline</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import { USER } from '../store';
-import { IS_ONLINE } from '../store/constants';
-import { loginRoute } from '../router/routes';
-
-export default {
-  name: 'Refresh',
-  computed: {
-    ...mapState([IS_ONLINE, USER]),
-  },
-  data() {
-    return {
-      isLogin: this.$router.currentRoute.path === loginRoute.path,
-    };
-  },
-};
+  import { mapState, mapActions } from 'vuex';
+  import { USER } from '../store';
+  import { IS_ONLINE, SYNC } from '../store/constants';
+  import { loginRoute } from '../router/routes';
+  export default {
+    name: 'Refresh',
+    computed: {
+      ...mapState([IS_ONLINE, USER]),
+    },
+    data() {
+      return {
+        isLogin: this.$router.currentRoute.path === loginRoute.path,
+      };
+    },
+    methods: {
+      ...mapActions({
+        sync: SYNC
+      }),
+      triggerSync() {
+        this.sync()
+      }
+    }
+  };
 </script>
 
 <style scoped lang="scss">
-@import "../scss/base";
-
-.is-online {
-  @include absolute(left 0 top 0);
-  @include m(3);
-
-  height: 10px;
-  width: 10px;
-  background-color: $red;
-  border-radius: 50%;
-  display: inline-block;
-}
-
-.is-online span {
-  margin-left: 20px;
-  margin-bottom: 25px;
-}
+  @import "../scss/base";
 
 .online {
-  background-color: $green;
-  line-height: inherit;
+  position: fixed;
+  width: 36px;
+  height: 30px;
+  right: 100px;
+  top: 38px;
+  cursor: pointer;
+}
+
+.offline {
+  position: fixed;
+  width: 36px;
+  height: 24px;
+  right: 100px;
+  top: 30px;
+  color: red;
 }
 </style>
-
