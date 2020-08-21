713bae025649e2abb376091a7c61f22eb3230561
Alan Sobócki <ctilhs@gmail.com>
2020-08-21 18:40:11 +0200

    Fix after CR

--- backend/pah_fm/permissions.py
+++ backend/pah_fm/permissions.py
@@ -1,11 +1,12 @@
 from rest_framework import permissions
 from fleet_management.models import User
 
+
 class UserHasGroupPermission(permissions.BasePermission):
-    message = 'You are not allowed to log in because you are not associated to proper group.'
+    message = 'You are not allowed to log in because you do not belong to any group. Please contact admins.'
 
     def has_permission(self, request, view):
         try:
-            return User.objects.get(email=request.data.get('username')).groups.exists()
+            return User.objects.get(username=request.data.get('username')).groups.exists()
         except User.DoesNotExist:
             return False
--- backend/pah_fm/views.py
+++ backend/pah_fm/views.py
@@ -2,6 +2,7 @@ from rest_framework_jwt.views import ObtainJSONWebToken
 from rest_framework_jwt.serializers import JSONWebTokenSerializer
 from pah_fm.permissions import UserHasGroupPermission
 
+
 class CustomObtainJSONWebToken(ObtainJSONWebToken):
     permission_classes = [UserHasGroupPermission]
     serializer_class = JSONWebTokenSerializer
