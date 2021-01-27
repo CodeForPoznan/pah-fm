11d0d3aa2627505ae31fc0fd997a43972b645139
Julia <j.anholcer@merixstudio.com>
2021-01-27 20:23:07 +0100

    Pah react/add bootstrap (#566)
    * Initial setup of react app
    
    * .gitignore changes
    
    * add some dependencies
    
    * reducers improvements, save token
    
    * move root folder
    
    * basic routing
    
    * get me slice
    
    * add react-bootstrap

--- frontend-react/src/views/Login/index.js
+++ frontend-react/src/views/Login/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 import Page from '../../components/Page';
 
 const LoginView = () => {
@@ -10,7 +14,28 @@ const LoginView = () => {
     <Page
       title="Login"
     >
-       {t('common.login')}
+      <Jumbotron>
+        <h2 className="header"> {t('common.login')}</h2>
+        <Form>
+          <Form.Group controlId="username">
+            <Form.Label>{t('login.username')}</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder={t('login.username')}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>{t('login.password')}</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder={t('login.password')}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {t('common.login')}
+          </Button>
+        </Form>
+      </Jumbotron>
     </Page>
   );
 };
