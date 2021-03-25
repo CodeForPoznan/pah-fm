92e764edeaaff5ed59ef6bcd1d72a518e23a93da
Artur Tamborski <tamborskiartur@gmail.com>
2021-03-25 23:55:21 +0100

    fix react warning on using key while listing items

--- frontend-react/src/routes.js
+++ frontend-react/src/routes.js
@@ -19,7 +19,7 @@ export const renderRoutes = (routes = []) => (
 
         return (
           <Route
-            key={route.path} 
+            key={route.key}
             path={route.path}
             exact={route.exact}
             render={(props) => (
@@ -42,22 +42,27 @@ const routes = [
   {
     exact: true,
     path: '/404',
+    key: 'NotFound',
     component: lazy(() => import('./views/NotFound'))
   },
   {
     exact: true,
     path: '/login',
+    key: 'Login',
     component: lazy(() => import('./views/Login'))
   },
   {
     exact: true,
     path: '/',
+    key: 'Home',
     component: lazy(() => import('./views/Home'))
   },
   {
     path: '*',
+    key: 'Default',
     routes: [
       {
+        key: 'Redirect',
         component: () => <Redirect to="/404" />
       }
     ]
