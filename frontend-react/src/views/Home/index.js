fb1d544c78b63299dda58488810b2ce2450f1ba3
Artur Tamborski <tamborskiartur@gmail.com>
2021-04-09 10:41:55 +0200

    Add missing spaces to statement

--- frontend-react/src/views/Home/index.js
+++ frontend-react/src/views/Home/index.js
@@ -5,8 +5,8 @@ import useT from '../../utils/translation';
 
 const statement =
   'The Polish Humanitarian Action is a Polish non-governmental organisation ' +
-  'which operates in Poland and other countries. Its mission is "to make the' +
-  'world a better place through alleviation of human suffering and promotion' +
+  'which operates in Poland and other countries. Its mission is "to make the ' +
+  'world a better place through alleviation of human suffering and promotion ' +
   'of humanitarian values".';
 
 const LoginView = () => {
