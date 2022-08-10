f0826e493596844be96c7c94a08a39a3168852cb
Dawid Wolski <merito@tuta.io>
2022-08-10 23:37:53 +0200

    Upgrade backend to Django 4 (#594)

--- backend/pah_fm/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework_jwt.views import ObtainJSONWebToken
-from rest_framework_jwt.serializers import JSONWebTokenSerializer
-from pah_fm.permissions import UserHasGroupPermission
-
-
-class CustomObtainJSONWebToken(ObtainJSONWebToken):
-    permission_classes = [UserHasGroupPermission]
-    serializer_class = JSONWebTokenSerializer
