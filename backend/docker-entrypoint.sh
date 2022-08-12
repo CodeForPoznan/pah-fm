0a7b16044bd35f74ec07acde8a8af34924af8395
Artur Tamborski <tamborskiartur@gmail.com>
2022-08-12 23:44:48 +0200

    Replace pip with pipenv in backend/ for CI migration

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-bash wait-for-it db:5432
+wait-for-it db:5432
 
-python3 manage.py migrate
-python3 manage.py create_admin hello@codeforpoznan.pl pass123 UA --django-admin
-python3 manage.py create_admin ola@pah.org.pl pass123 UA
-python3 manage.py runserver 0.0.0.0:8000
+./manage.py migrate
+./manage.py create_admin hello@codeforpoznan.pl pass123 UA --django-admin
+./manage.py runserver 0.0.0.0:8000
