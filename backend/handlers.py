a84b3bfecf00c4a8eba8f3c8c29b3c9c159c8fba
Artur Tamborski <tamborskiartur@gmail.com>
2023-01-27 03:11:15 +0100

    Test pipeline changes

--- backend/handlers.py
+++ backend/handlers.py
@@ -12,7 +12,6 @@ from pah_fm.wsgi import application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pah_fm.settings")
 django.setup()
 
-
 def api(event, context):
     event["headers"]["X-Forwarded-Host"] = os.environ["BASE_URL"]
     print("REQUEST: ", event)
