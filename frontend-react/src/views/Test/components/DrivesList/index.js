404e7ec2afdb206b7efd6623a529dffdad29c0ca
cezary-janicki <cezary.d.janicki@gmail.com>
2023-07-04 11:25:32 +0200

    added mock data to the accordion menus

--- frontend-react/src/views/Test/components/DrivesList/index.js
+++ frontend-react/src/views/Test/components/DrivesList/index.js
@@ -5,6 +5,7 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { mockDrives } from './mockItems';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,6 +22,55 @@ export default function DrivesList() {
 
   return (
     <div className={classes.root}>
+      {mockDrives.map((drive, index) => (
+        <Accordion key={index}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>
+              {drive.date}
+              {' '}
+              From
+              {' '}
+              {drive.start_location}
+              {' '}
+              to
+              {' '}
+              {drive.end_location}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <p>
+                {'Description: '}
+                {drive.description}
+              </p>
+              <p>
+                {'With car: '}
+                {drive.car_plates}
+              </p>
+              <p>
+                {'Passenger: '}
+                {drive.passenger}
+              </p>
+              <p>
+                {'Project: '}
+                {drive.project_title}
+              </p>
+              <p>
+                {'Starting milage: '}
+                {drive.start_mileage}
+              </p>
+              <p>
+                {'Ending milage: '}
+                {drive.end_mileage}
+              </p>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -35,30 +85,6 @@ export default function DrivesList() {
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography className={classes.heading}>Disabled Accordion</Typography>
-        </AccordionSummary>
       </Accordion>
     </div>
   );
